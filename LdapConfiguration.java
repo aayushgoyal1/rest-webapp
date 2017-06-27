25330f76872af3c1792b52124c47a2b9283cdc12
Aayush Goyal <goyalaayush@yahoo.com>
2017-06-27 10:33:49 -0500

    Update LdapConfiguration.java

--- LdapConfiguration.java
+++ LdapConfiguration.java
@@ -1,1 +1,26 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.LdapContextSource;
 
+@Configuration
+public class LdapConfiguration {
+
+	private String ldapUrl=Constants.getString("ldap.client.url");
+
+	@Bean
+    public LdapContextSource contextSource(){
+        LdapContextSource contextSource = new LdapContextSource();
+        contextSource.setUrl(ldapUrl);
+		contextSource.afterPropertiesSet();
+        return contextSource;
+    }
+
+    @Bean
+    public LdapTemplate ldapTemplate(){
+        LdapTemplate template = new LdapTemplate(contextSource());
+        return template;
+    }
+
+
+}
